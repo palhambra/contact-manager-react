1be7c418600987bb9ae63fd4d861afdc91bbe308
Paolo <paolo.alhambra@yahoo.com>
2023-09-21 21:51:37 -0700

    contact list

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,9 +1,29 @@
-
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import AddContact from './components/AddContact'
+import ContactList from './components/ContactList'
 
 function App() {
+
+  const contacts = [
+    {
+      id: '1',
+      name: 'Paolo',
+      email: 'paolo@gmail.com'
+    },
+    {
+      id: '2',
+      name: 'Mia',
+      email: 'mia@gmail.com'
+    }
+  ]
   return (
-    <div>Hello World</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts}/>
+    </div>
   );
 }
 
